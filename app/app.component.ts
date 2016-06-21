38ee6d51576d4297ecc9aaa83b0036a3b93eb848
Alfredo Rosadilla <arosadilla@altimetrik.com>
2016-06-21 14:44:27 -0300

    Master/Detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,37 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  templateUrl: '../app/templates/heroDetail.html',
+  styleUrls: ['../app/styles/style.css']
 })
+export class AppComponent {
+  public heroes = HEROES;
+  public selectedHero: Hero;
 
-export class AppComponent { }
+  public onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
+
+export class Hero {
+  public id: number;
+  public name: string;
+
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
